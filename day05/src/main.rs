5f25c4d302a12ae08d61f5bc386033eab7c524e5
3v0k4 <riccardo.odone@gmail.com>
2020-12-05 22:48:05 +0100

    day05

---
+++ day05/src/main.rs
@@ -0,0 +1,67 @@
+fn main() {
+  let file: String = std::fs::read_to_string("./input.txt")
+    .expect("Something went wrong reading the file");
+  let split: Vec<_> = file.split("\n").filter(|x| x.len() == 10).collect();
+  let seats = seats(split);
+  let max = seats.iter().max().unwrap();
+  println!("{}", max);
+
+  let min = seats.iter().min().unwrap();
+  for i in *min..*max {
+    if !seats.iter().any(|s| *s == i) {
+      println!("{}", i);
+      break;
+    };
+  }
+}
+
+fn seats(css: Vec<&str>) -> Vec<u32> {
+  css.iter().map(|cs| seat(cs)).collect()
+}
+
+fn seat(cs: &str) -> u32 {
+  let (r, c) = cs.split_at(7);
+  row(r) * 8 + col(c)
+}
+
+fn row(cs: &str) -> u32 {
+  let bits: String = cs.chars().map(|c| {
+    match c {
+      'B' => '1',
+      'F' => '0',
+      _   => 'X'
+    }
+  }).collect();
+  u32::from_str_radix(&bits, 2).unwrap()
+}
+
+fn col(cs: &str) -> u32 {
+  let bits: String = cs.chars().map(|c| {
+    match c {
+      'R' => '1',
+      'L' => '0',
+      _   => 'X'
+    }
+  }).collect();
+  u32::from_str_radix(&bits, 2).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn seat_BFFFBBFRRR() {
+    assert_eq!(seat("BFFFBBFRRR"), 567);
+  }
+
+  #[test]
+  fn row_BFFFBBF() {
+    assert_eq!(row("BFFFBBF"), 70);
+  }
+
+  #[test]
+  fn col_RRR() {
+    assert_eq!(col("RRR"), 7);
+  }
+}
